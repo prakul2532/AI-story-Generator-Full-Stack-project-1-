8c29a4bbb3e445e3d32e48aa20b7f12044035edc
Student-Prakul <prakul25.doe.svc.du@gmail.com>
2026-02-26 20:32:30 +0530

    deploying my app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,7 +20,7 @@ function Index(){
   const handleGenerate = async () => {
     try {
       setLoading(true);
-      const res = await fetch("http://127.0.0.1:8000/generate-story",{
+      const res = await fetch("https://ai-story-generator-2oru.onrender.com",{
         method: "POST",
         headers: {
           "Content-Type": "application/json"
